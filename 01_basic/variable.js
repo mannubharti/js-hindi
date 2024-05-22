96e520a576bf67acc66270881e0d4be2afaaeecb
Mannu bharti <106397290+mannubharti@users.noreply.github.com>
2024-05-22 22:27:26 +0000

    variable.js

---
+++ 01_basic/variable.js
@@ -0,0 +1,7 @@
+const accountId = 144453
+let accountEmail = "hc@hc.com"
+accountPassword = "212121"
+accountCity = "Bengaluru"
+
+console.log(accountId);
+console.table([acccountId, accountEmail, accountPassword, accountCity]);
