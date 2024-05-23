5cc980c3fbccaeac54fc085f13f7781940bf5d40
Mannu bharti <106397290+mannubharti@users.noreply.github.com>
2024-05-23 13:03:27 +0000

    conversion of datatypes

--- .devcontainer/01_basic/variable.js
+++
@@ -1,7 +0,0 @@
-const accountId = 144453
-let accountEmail = "hc@hc.com"
-accountPassword = "212121"
-accountCity = "Bengaluru"
-
-console.log(accountId);
-console.table([acccountId, accountEmail, accountPassword, accountCity]);
